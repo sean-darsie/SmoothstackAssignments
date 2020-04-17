80e81c758953c4a6474f485b8fc136b015e6d4fa
Sean Darsie <sdarsie12@gamil.com>
2020-04-17 12:12:46 -0700

    Initial commit lms project

---
+++ JavaAssignments/src/com/smoothstack/training/libraryproject/LibraryInterface.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.smoothstack.training.libraryproject;
+
+/**
+ * @author seandarsie
+ *
+ */
+public class LibraryInterface {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// here we go. we're going to have a library record system interface
+
+	}
+
+}
---
+++ JavaAssignments/src/com/smoothstack/training/libraryproject/Publisher.java
@@ -0,0 +1,46 @@
+/**
+ * 
+ */
+package com.smoothstack.training.libraryproject;
+
+/**
+ * @author seandarsie
+ *
+ */
+public class Publisher {
+	private int publisherId;
+	private String name;
+	private String address;
+	
+	public Publisher(int publisherId, String name, String address) {
+		this.setPublisherId(publisherId);
+		this.setName(name);
+		this.setAddress(address);
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getPublisherId() {
+		return publisherId;
+	}
+
+	public void setPublisherId(int publisherId) {
+		this.publisherId = publisherId;
+	}
+	
+	
+}
